8037ba6104ac101c72c42ff38f60fde24f14f152
Christopher Funderburk <cfunderburk@shipt.com>
2019-04-17 20:39:23 -0500

    Initiate Project

---
+++ scripts/index.js
@@ -0,0 +1,16 @@
+let state = {
+  player1: {
+    name: '',
+    ships: [
+      { 0: [[x, y], [x, y]] },
+      { 1: [[x, y], [x, y]] },
+      { 2: [[x, y], [x, y], [x, y]] },
+      { 3: [[x, y], [x, y], [x, y]] },
+      { 4: [[x, y], [x, y], [x, y], [x, y]] },
+    ]
+  },
+
+  gameStatus: '',
+
+}
+
