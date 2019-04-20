f450d1f312b801ae1cbc63faa368ad8f39fcc722
Christopher Funderburk <cfunderburk@shipt.com>
2019-04-19 20:23:04 -0500

    Add Welcome Message, Refactor Update State & Game Mode Selection

--- scripts/index.js
+++ scripts/index.js
@@ -8,7 +8,8 @@ const gameState = () => {
       name: null,
       gameBoard: setBoard()
     },
-    multiplayer: null,
+
+    gameMode: null,
     currentPlayer: 'player1'
   });
 }
@@ -34,35 +35,37 @@ const cols = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
 const setBoard = () => [...Array(size)].map(() => [...Array(size).fill(null)])
 
 
-const makePlay = (state, row, column, player) => {
-  board = state[player].gameBoard
-  if (!board[row][column]) {
-    // console.log("Miss!")
-    updatedBoard = [...board]
-    updatedBoard[row][column] = "M"
-    return state = { ...state, [player]: { gameBoard: updatedBoard } }
-  } else if (board[row][column] == "M") {
-    console.log("You have already picked this square")
-  } else if (board[row][column] == "H") {
-    console.log("You have already picked this square")
-  }
-}
+// const makePlay = (state, row, column, player) => {
+//   board = state[player].gameBoard
+//   if (!board[row][column]) {
+//     // console.log("Miss!")
+//     updatedBoard = [...board]
+//     updatedBoard[row][column] = "M"
+//     return state = { ...state, [player]: { gameBoard: updatedBoard } }
+//   } else if (board[row][column] == "M") {
+//     console.log("You have already picked this square")
+//   } else if (board[row][column] == "H") {
+//     console.log("You have already picked this square")
+//   }
+// }
 
 let state = gameState()
 let ships = shipState()
-console.log(ships)
 
+const updateState = (slice, data) => {
+  state = { ...state, [slice]: data }
+}
 
 let button = document.getElementById('actionButton')
 button.addEventListener('click', () => {
-  state = changePlayer(state)
+  changePlayer()
 })
 
-const changePlayer = (state) => {
-  let currentPlayer = document.getElementById('currentPlayer')
+const changePlayer = () => {
+  let heading = document.getElementById('headerStatus')
   let nextPlayer = state.currentPlayer == "player1" ? 'player2' : "player1"
-  currentPlayer.innerText = nextPlayer
-  return { ...state, currentPlayer: nextPlayer }
+  heading.innerText = nextPlayer
+  updateState('currentPlayer', nextPlayer)
 }
 
 // const placeShips = (ships, player, position) => {
@@ -85,7 +88,7 @@ const createSquare = (elem, col, row) => {
   let newSquare = document.createElement('div')
   newSquare.classList.add('square')
   newSquare.setAttribute('id', `${col}${row}`)
-  newSquare.addEventListener('click', (e) => (console.log(e.target.id)))
+  newSquare.addEventListener('click', (e) => (console.log(e.target.id,state)))
   elem.appendChild(newSquare)
 }
 
@@ -97,11 +100,48 @@ const createRow = (elem, rowId) => {
 }
 
 const createBoard = () => {
+  let wC = document.getElementById('wc')
+  wC.parentNode.removeChild(wC)
   const board = document.getElementById('board')
   rows.map((row) => createRow(board, row))
 }
 
 
+const pickGameModeButton = (obj, text, mode) => {
+  let button = document.createElement('button')
+  let buttonTxt = document.createTextNode(text)
+  button.appendChild(buttonTxt)
+  button.classList.add('button')
+  button.setAttribute('id', mode)
+  button.addEventListener('click', () => {
+    updateState('gameMode', button.id)
+    createBoard()
+    console.log(button.id, state)
+  })
+  obj.appendChild(button)
+}
+
+const welcomeScreen = () => {
+  const board = document.getElementById('board')
+  let welcomeContainer = document.createElement('div')
+  welcomeContainer.classList.add('welcome-container')
+  welcomeContainer.setAttribute('id', 'wc')
+  let welcomeStatement = document.createElement('h1')
+  let welcomeText = document.createTextNode('Welcome to BattleShipt')
+  let subStatement = document.createElement('p')
+  let subText = document.createTextNode('Before we start the game...blah,blah')
+  welcomeStatement.appendChild(welcomeText)
+  subStatement.appendChild(subText)
+  welcomeContainer.appendChild(welcomeStatement)
+  welcomeContainer.appendChild(subStatement)
+  pickGameModeButton(welcomeContainer, '2 Players', '2P')
+  pickGameModeButton(welcomeContainer, 'Vs. Comp', 'C')
+  board.appendChild(welcomeContainer)
+
+
+}
+
 window.addEventListener('DOMContentLoaded', () => {
-  createBoard()
+  welcomeScreen()
+  // 
 })
